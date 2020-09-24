db176442e66d87e03a7bed4aa8288b899f903c31
Etukuri Sreelakshmi <etukurisreelakshmi@gmail.com>
2020-09-24 19:40:19 +0530

    password

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
+import Validation from './Validation';
+import Char from './Char';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component{
+ constructor(props){
+     super(props);
+     this.state={message:'',type:"password"};
+ }
+ Test=(value)=>{
+     let y=this.state.message+value;
+     console.log(typeof(this.state.message));
+    this.setState({message:y})
+ };
+Test1=()=>{
+    let x=[1,2,3,4,5,6,7,8,9];
+    let a=x.map(item=>{
+    return <button className="grid-item" onClick={()=>this.Test(item)}>{item}</button>
+    });
+    return a;
+};
+Delete=()=> {
+    let b = this.state.message.split('');
+    b.pop();
+    this.setState({message:b.join('')})
+    //console.log(b.join(''));
 
+};
+Show=()=>{
+   if(this.state.type==='password'){
+       this.setState({type:'text'})
+   }
+   else{
+       this.setState({type:'password'})
+   }
+};
+ render(){
+     return(
+             <div>
+             <input type={this.state.type} value={this.state.message}/><br/>
+
+                 <div className="grid-container">{this.Test1()}</div>
+                 <button onClick={()=>this.Delete()}>Delete</button>
+                 <button onClick={()=>this.Show()}>Show</button>
+
+         </div>
+     )
+ }
+}
 export default App;
